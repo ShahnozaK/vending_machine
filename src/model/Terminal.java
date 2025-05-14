5028ce9a254b592cedd86555ca041525517e2fdb
Shahnoza_K <shahnoza.kalysbekova@gmail.com>
2025-05-14 14:59:14 +0600

    Добавила класс Терминал, CoinAcceptor, переписала метод в ранее написанном классе.

--- src/model/Terminal.java
+++ src/model/Terminal.java
@@ -1,10 +1,18 @@
 package model;
 
-public class Terminal {
+public class Terminal extends PaymentMethod {
     private int cardNumber;
     private int password;
-    private int amountOfMoney;
 
-    public Terminal() {
+    public Terminal(int amount, int cardNumber, int password) {
+        super(amount);
+        this.cardNumber = cardNumber;
+        this.password = password;
+    }
+
+
+    @Override
+    boolean pay(int amount) {
+        return false;
     }
 }
